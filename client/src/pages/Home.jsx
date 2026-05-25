9a36f21d1ce389f20afb8bac6497f51a8f0f556a
NAVNEEL-jpg <navneeldutta@gmail.com>
2026-05-26 02:36:20 +0530

    increase brand logo size

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -106,7 +106,7 @@ const BrandLogos = memo(function BrandLogos() {
                 <img
                   src={src}
                   alt=""
-                  style={{ height: 48, width: "auto", objectFit: "contain", filter: "invert(1)" }}
+                 style={{ height: 48, width: "120px", objectFit: "contain", filter: "invert(1)" }}
                 />
               </span>
             ))}
